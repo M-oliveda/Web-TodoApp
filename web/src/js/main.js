82ece13440a7a32ba58ea1eb25cab48aa60e3228
Mauricio Oliveda <maurioliveda@gmail.com>
2023-09-05 18:41:28 -0300

    :construction: Work inside the main section. Added the main functions and styles.

--- web/src/js/main.js
+++ web/src/js/main.js
@@ -47,6 +47,64 @@ function filterTasks(typeTask) {
   }
 }
 
+function addNewTask() {
+  const newTaskContainer = document.createElement("li");
+  const newTask = document.createElement("div");
+  const newTaskIcon = document.createElement("button");
+  const newTaskText = document.createElement("input");
+  const newTaskDeleteIcon = document.createElement("button");
+
+  newTask.classList.add("task");
+  newTaskIcon.classList.add("task__icon");
+  newTaskText.classList.add("task__text");
+  newTaskDeleteIcon.classList.add("task__delete-icon");
+
+  newTask.setAttribute("taskId", (window.sessionStorage.length + 1).toString());
+
+  newTaskIcon.addEventListener("click", (e) => {
+    e.target.classList.toggle("task__icon-completed");
+    toggleTaskCompletion(e.target.parentElement.getAttribute("taskId"));
+  });
+
+  newTaskText.addEventListener("focusout", (e) => {
+    const taskInfo = {
+      taskId: e.target.parentElement.getAttribute("taskId"),
+      isCompleted: false,
+      value: e.target.value,
+    };
+
+    window.sessionStorage.setItem(
+      e.target.parentElement.getAttribute("taskId"),
+      JSON.stringify(taskInfo),
+    );
+  });
+
+  newTaskDeleteIcon.addEventListener("click", (e) => {
+    e.target.parentElement.parentElement.remove();
+    deleteTask(e.target.parentElement.getAttribute("taskId"));
+  });
+
+  newTaskContainer.append(newTask);
+  newTask.append(newTaskIcon, newTaskText, newTaskDeleteIcon);
+
+  document.querySelector(".tasks").appendChild(newTaskContainer);
+
+  newTaskText.focus();
+}
+
+function toggleTaskCompletion(taskId) {
+  const taskObj = JSON.parse(window.sessionStorage.getItem(taskId));
+
+  window.sessionStorage.setItem(
+    taskId,
+    JSON.stringify({ ...taskObj, isCompleted: !taskObj.isCompleted }),
+  );
+}
+
+function deleteTask(taskId) {
+  window.sessionStorage.removeItem(taskId);
+}
+
 window.addEventListener("load", () => setTimeCalendar());
 
 document.querySelector("#filterBoxButton").addEventListener("click", () => {
@@ -64,3 +122,7 @@ document
   .forEach((e) =>
     e.addEventListener("click", () => filterTasks(e.innerText.toLowerCase())),
   );
+
+document
+  .querySelector("#addNewTask")
+  .addEventListener("click", () => addNewTask());
